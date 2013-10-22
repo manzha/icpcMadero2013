3e16e4b0314eff344eca7d622514425f0e07f578
manzha <jjgmancha@gmail.com>
2013-10-22 01:02:49 -0500

    first

---
+++ header.php
@@ -0,0 +1,24 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width" />
+    <title>ICPC Madero 2013</title>
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/custom.modernizr.js"></script>
+</head>
+<body>
+
+    <div class="row">
+        <div class="large-4 columns">
+            <h1><img src="img/acm-icpc-logo.gif"></h1>
+        </div>
+        <div class="large-8 columns">
+            <ul class="right button-group">
+                <li><a href="index.php" class="button">Inicio</a></li>
+                <li><a href="quees.php" class="button">¿Qué es?</a></li>
+                <li><a href="#" class="button">Registro</a></li>
+                <li><a href="hotelSede.php" class="button">Hotel Sede</a></li>
+                <li><a href="contacto.php" class="button">Contacto</a></li>
+            </ul>
+        </div>
+    </div>
