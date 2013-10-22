c0167082799857b3b45385ad4d882bea9bbb1471
manzha <jjgmancha@gmail.com>
2013-10-22 00:25:45 -0500

    first

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <?php
+    include("header.php");
+    ?>
+
+        <!-- End Header and Nav -->
+
+        <!-- First Band (Slider) -->
+
+        <div class="row">
+            <div class="large-12 columns">
+                <!--<div id="slider">-->
+                    <!--<img src="http://placehold.it/1000x400&text=[ img 1 ]" />-->
+                <!--</div>-->
+                <ul data-orbit>
+                    <li><img src="http://placehold.it/1000x400&text=[ img 1 ]" /></li>
+                    <li><img src="http://placehold.it/1000x400&text=[ img 2 ]" /></li>
+                    <li><img src="http://placehold.it/1000x400&text=[ img 3 ]" /></li>
+                    <li><img src="http://placehold.it/1000x400&text=[ img 4 ]" /></li>
+                </ul>
+
+                <hr />
+            </div>
+        </div>
+
+        <!-- Three-up Content Blocks -->
+
+        <div class="row">
+            <div class="large-4 columns">
+                <img src="http://placehold.it/400x300&text=[img]" />
+                <h4>This is a content section.</h4>
+                <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+            </div>
+
+            <div class="large-4 columns">
+                <img src="http://placehold.it/400x300&text=[img]" />
+                <h4>This is a content section.</h4>
+                <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+            </div>
+
+            <div class="large-4 columns">
+                <img src="http://placehold.it/400x300&text=[img]" />
+                <h4>This is a content section.</h4>
+                <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+            </div>
+
+        </div>
+
+        <!-- Call to Action Panel -->
+        <div class="row">
+            <div class="large-12 columns">
+
+                <div class="panel">
+                    <h4>¡Estamos para servirte!</h4>
+
+                    <div class="row">
+                        <div class="large-9 columns">
+                            <p>Nos encantaría escuchar tus dudas o sugerencias del evento.</p>
+                        </div>
+                        <div class="large-3 columns">
+                            <a href="#" class="radius button right">Contactanos</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- Footer -->
+        <?php
+        include ("footer.php");
+        ?>
+    
+</html>
